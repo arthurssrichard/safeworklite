53da965e4293bb84a6c4e867b9f8e94862b16f41
arthurssrichard <arthurssrichard@gmail.com>
2024-10-02 19:35:31 -0300

    setups bat e sh atualizados

--- src/db/setup.bat
+++ src/db/setup.bat
@@ -4,7 +4,7 @@ set DB_PASS=
 set DB_HOST=127.0.0.1
 
 :: Execute o arquivo SQL
-mysql -u %DB_USER% -p%DB_PASS% -h %DB_HOST% < db\setup.sql
+mysql -u %DB_USER% -p%DB_PASS% -h %DB_HOST% < db\safeworklite-dump.sql
 
 echo Banco de dados e tabelas criados com sucesso.
 pause
--- src/db/setup.sh
+++ src/db/setup.sh
@@ -6,6 +6,6 @@ DB_PASS=""
 DB_HOST="127.0.0.1"
 
 # Execute o arquivo SQL para criar o banco de dados e tabelas
-mysql -u $DB_USER -p$DB_PASS -h $DB_HOST < db/setup.sql
+mysql -u $DB_USER -p$DB_PASS -h $DB_HOST < db/safeworklite-dump.sql
 
 echo "Banco de dados e tabelas criados com sucesso."
