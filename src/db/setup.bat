e720b93d80a4a488d72466233d60a698a993bdae
arthurssrichard <141867186+arthurssrichard@users.noreply.github.com>
2024-10-02 20:04:36 -0300

    .bat corrigido e gitignore adicionado

--- src/db/setup.bat
+++ src/db/setup.bat
@@ -3,8 +3,14 @@ set DB_USER=root
 set DB_PASS=
 set DB_HOST=127.0.0.1
 
-:: Execute o arquivo SQL
-mysql -u %DB_USER% -p%DB_PASS% -h %DB_HOST% < db\safeworklite-dump.sql
+:: Defina o caminho correto para o arquivo SQL (relativo à pasta do script)
+set SQL_FILE=%~dp0..\..\src\db\safeworklite-dump.sql
+
+:: Defina o caminho completo para o executável mysql
+set MYSQL_PATH=C:\xampp\mysql\bin\mysql.exe
+
+:: Execute o arquivo SQL usando o caminho completo para o mysql e a variável SQL_FILE
+"%MYSQL_PATH%" -u %DB_USER% -p%DB_PASS% -h %DB_HOST% < "%SQL_FILE%"
 
 echo Banco de dados e tabelas criados com sucesso.
 pause
