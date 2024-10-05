71355cbef23b919cc2b7b69c93df36543a37e1d4
arthurssrichard <141867186+arthurssrichard@users.noreply.github.com>
2024-10-05 11:21:21 -0300

    setup.bat corrigido

--- src/db/setup.bat
+++ src/db/setup.bat
@@ -9,8 +9,12 @@ set SQL_FILE=%~dp0..\..\src\db\safeworklite-dump.sql
 :: Defina o caminho completo para o executável mysql
 set MYSQL_PATH=C:\xampp\mysql\bin\mysql.exe
 
-:: Execute o arquivo SQL usando o caminho completo para o mysql e a variável SQL_FILE
-"%MYSQL_PATH%" -u %DB_USER% -p%DB_PASS% -h %DB_HOST% < "%SQL_FILE%"
+:: Cria o banco de dados, se não existir
+"%MYSQL_PATH%" -u %DB_USER% -p%DB_PASS% -h %DB_HOST% -e "CREATE DATABASE IF NOT EXISTS safeworklite;"
+
+:: Executa o arquivo SQL no banco de dados safeworklite
+"%MYSQL_PATH%" -u %DB_USER% -p%DB_PASS% -h %DB_HOST% safeworklite < "%SQL_FILE%"
+
 
 echo Banco de dados e tabelas criados com sucesso.
 pause
