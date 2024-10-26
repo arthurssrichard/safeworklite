567921cc576553bdb4d1ad8183698d4bde36ec1d
arthurssrichard <arthurssrichard@gmail.com>
2024-10-26 17:52:08 -0300

    Update setup.sh

--- src/db/setup.sh
+++ src/db/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Defina os parâmetros de conexão do MySQL/MariaDB
-DB_USER="labdatabase"
-DB_PASS="lab@Database2022"
-DB_HOST="127.0.0.1"
+DB_USER="root"
+DB_PASS=""
+DB_HOST="localhost"
 DB_PORT="3306"  # Porta correta
 DB_NAME="safeworklite"  # Nome do banco de dados
 
@@ -12,7 +12,7 @@ SQL_FILE="$(dirname "$0")/../../src/db/safeworklite-dump.sql"
 SEED_FILE="$(dirname "$0")/../../src/db/seeder.sql"
 
 # Defina o caminho completo para o executável MySQL no CentOS
-MYSQL_PATH="/usr/bin/mysql"
+MYSQL_PATH="/opt/lampp/bin/mysql"
 
 # Primeiro, crie o banco de dados, se não existir
 "$MYSQL_PATH" -u "$DB_USER" -p"$DB_PASS" -h "$DB_HOST" --port="$DB_PORT" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
